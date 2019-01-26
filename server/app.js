3b914f77c6029ee175beac9167efa6bd219eb47d
Olivia Powell <olive.p@outlook.com>
2019-01-26 13:17:21 -0500

    added nodemon and updated scripts

--- server/app.js
+++ server/app.js
@@ -12,9 +12,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(express.static(path.join(__dirname, '../client/build/')));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 module.exports = app;

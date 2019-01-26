a78beea263bea7ad2ec2b80c13212c8993ceb640
Olivia Powell <olive.p@outlook.com>
2019-01-26 13:33:31 -0500

    fixed express use ordering

--- server/app.js
+++ server/app.js
@@ -12,11 +12,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../client/build/')));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(express.static(path.join(__dirname, '../client/build/')));
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });

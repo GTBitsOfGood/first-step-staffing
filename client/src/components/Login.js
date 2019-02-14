73461d726f620c83c3f7875468de1f5c6bc8ff15
Jason Lee <jason.khn.lee@gmail.com>
2019-02-14 18:22:01 -0500

    Added two text inputs, very plain, vv basic

---
+++ client/src/components/Login.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
+});
+
+class Login extends React.Component {
+    state = {
+        lastName: '',
+        ssn: ''
+    };
+
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.value });
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <form className={classes.container} noValidate autoComplete="off">
+                <TextField
+                    required
+                    id="standard-name"
+                    label="Last Name"
+                    className={classes.textField}
+                    value={this.state.lastName}
+                    onChange={this.handleChange('lastName')}
+                    margin="normal"
+                />
+
+                <TextField
+                    required
+                    id="standard-number"
+                    label="Last Four Digits of SSN"
+                    value={this.state.ssn}
+                    onChange={this.handleChange('ssn')}
+                    type="number"
+                    className={classes.textField}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    margin="normal"
+                />
+            </form>
+        );
+    }
+}
+
+Login.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);

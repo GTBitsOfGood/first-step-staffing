e1fb679576cb13487db71b33a0ac981def99b523
Justin Kavalan <justinkavalan@gmail.com>
2019-02-14 18:17:33 -0500

    Barebones registration page

--- client/src/pages/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage.jsx
@@ -1,8 +1,39 @@
 import React, { Component } from 'react'
+import { FormLabel, Input, FormGroup } from '@material-ui/core';
 
 class RegistrationPage extends Component {
   render() {
-    return <div> This is registration </div>
+    //   Firstname last name SSN BDay
+    let styles = {
+        margin: 'auto',
+    }
+
+    let submitStyle = {
+        width: 'auto',
+        margin: 'auto',
+    }
+    return <div style={styles}>
+    <FormGroup>
+{/* TODO: Fix bad code style */}
+        <FormLabel style={styles}>
+            First Name:
+            <Input type="text" name="first" />
+        </FormLabel>
+        <FormLabel style={styles}>
+            Last Name:
+            <Input type="text" name="last" />
+        </FormLabel>
+        <FormLabel style={styles}>
+            SSN:
+            <Input type="password" name="SSN" />
+        </FormLabel> 
+        <FormLabel style={styles}>
+            Birth date:
+            <Input type="date" name="birthdate"/>
+        </FormLabel>
+        <Input type="submit" value="Submit" style={submitStyle} />
+        </FormGroup>
+    </div>
   }
 }
 

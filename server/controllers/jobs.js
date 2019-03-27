f0082d242eab3df46f6d857e71d4220eff17823f
Jason Lee <jason.khn.lee@gmail.com>
2019-03-26 20:18:55 -0400

    Finds Atlanta for default location

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -5,19 +5,23 @@ export function create(req, res, next) {
   if (req.body.name && req.body.peopleNeeded && req.body.transportationType
       && req.body.transportationCost) {
     
-
-    const job = {
-      name: req.body.name,
-      location: "5c61cb581ef33c0e3823897d",
-      peopleNeeded: req.body.peopleNeeded,
-      transportationType: req.body.transportationType,
-      transportationCost: req.body.transportationCost
-    }
-
-    Job.create(job, (err, j) => {
-      if (err) return next(err)
-      return res.status(201).json({
-        job: j
+    let id
+    const atl = Location.findOne({"city": "Atlanta"}, (err, item) => {
+      id = item["_id"];
+      console.log(item);
+      console.log(id)
+      const job = {
+        name: req.body.name,
+        location: id,
+        peopleNeeded: req.body.peopleNeeded,
+        transportationType: req.body.transportationType,
+        transportationCost: req.body.transportationCost
+      }
+      Job.create(job, (err, j) => {
+        if (err) return next(err)
+        return res.status(201).json({
+          job: j
+        })
       })
     })
   }

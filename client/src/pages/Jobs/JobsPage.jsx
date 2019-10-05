0ee24546900fa41c1ca18bb87c33e2b936197c80
Varshita Patakottu <varshita13@gmail.com>
2019-10-05 18:48:26 -0400

    Add dropdown with dummy jobs

--- client/src/pages/Jobs/JobsPage.jsx
+++ client/src/pages/Jobs/JobsPage.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import CustomTable from '../../components/tables/CustomTable'
 import Button from '@material-ui/core/Button'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import InputLabel from '@material-ui/core/InputLabel'
 import { withStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
@@ -15,6 +18,10 @@ const styles = theme => ({
   title: {
     color: theme.palette.secondary.main,
     textAlign: 'center'
+  },
+  dropDown: {
+    minWidth: 120,
+
   }
 })
 
@@ -22,7 +29,8 @@ class JobPage extends Component {
   state = {
     jobs: [],
     jobsLoading: false,
-    jobsError: false
+    jobsError: false,
+    jobAssigned: ''
   }
 
   componentDidMount() {
@@ -41,6 +49,12 @@ class JobPage extends Component {
 
   rowClick = (e, id) => {
     this.props.history.push(`${routes.JOBDETAIL}${id}`)
+  }
+
+  handleChange = event => {
+    this.setState({
+      jobAssigned: event.target.value
+    });
   }
 
   render() {
@@ -68,6 +82,25 @@ class JobPage extends Component {
         >
           Create New Job
         </Button>
+        <br></br>
+
+        <InputLabel htmlFor="JobAssignments"> Job Assigned: </InputLabel>
+        <Select
+          value={this.state.jobAssigned}
+          className={classes.dropDown}
+          onClick={this.handleChange}
+          inputProps={{id: 'JobAssignments'}}
+          >
+            <MenuItem value="Job1"> Painting </MenuItem>
+            <MenuItem value="Job2"> Mowing </MenuItem>
+            <MenuItem value="Job3"> Factory work </MenuItem>
+            <MenuItem value="Job4"> Food Critic </MenuItem>
+        </Select>
+
+
+
+
+
       </div>
     )
   }

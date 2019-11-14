211cfd39cf7341c584bbc4fa9de5872cf4629ef2
Adeeb Zaman <zamanadeeb789@gmail.com>
2019-11-14 13:43:26 -0500

    Added lists of employees into job accordions

--- client/src/pages/DispatchPage/DispatchPage.jsx
+++ client/src/pages/DispatchPage/DispatchPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Button, SideNav,SideNavItems, SideNavMenuItem, Tile, Form, TextInput, FormLabel, DatePicker, DatePickerInput, Select, SelectItem, SelectItemGroup, Accordion, AccordionItem } from 'carbon-components-react'
-import CheckinPage from "../CheckinPage/CheckinPage.jsx"
+import { Button, SideNav,SideNavItems, SideNavMenuItem, StructuredListWrapper, StructuredListBody, StructuredListRow, StructuredListCell, StructuredListInput, StructuredListHead, Accordion, AccordionItem } from 'carbon-components-react'
 import "./DispatchPage.scss"
 
 
@@ -10,20 +9,16 @@ class DispatchPage extends React.Component {
         super(props);
         this.state = {
             allData: {},
-            clickCheckin: false
         }
     }
     componentDidMount(){
         fetch('/api/dispatch/getEmployeesByJobs')
         .then(response => response.json())
         .then(data => this.setState({allData:data}))
-
+        
     }
 
     goToCheckIn = event => {
-        // this.setState({
-        //     clickCheckin: true
-        // })
         window.location.href = "/checkin"
     }
 
@@ -53,7 +48,7 @@ class DispatchPage extends React.Component {
 
                     </div>
                     <Button
-                   
+                    className="check-in-button"
                     disabled={false}
                     kind="primary"
                     tabIndex={1}
@@ -62,19 +57,109 @@ class DispatchPage extends React.Component {
                 </div>
                 <div className="accordion-container">
                     <Accordion>
-                        {keys.map(key => (<AccordionItem title={key}> </AccordionItem>))}
-                        
-
-
+                        {keys.map(key => (<AccordionItem title={key}>
+                            <StructuredListWrapper
+                                ariaLabel="List of users"
+                                border
+                                selection
+                            >
+                                <StructuredListHead>
+                                    <StructuredListRow
+                                        head
+                                        label={false}
+                                        onKeyDown={function noRefCheck(){}}
+                                        tabIndex={0}
+                                    >
+                                    <StructuredListCell
+                                        head
+                                        noWrap={false}
+                                    >
+                                        #
+                                    </StructuredListCell>
+                                    <StructuredListCell
+                                        head
+                                        noWrap={false}
+                                    >
+                                        First
+                                    </StructuredListCell>
+                                    <StructuredListCell
+                                        head
+                                        noWrap={false}
+                                    >
+                                        Last
+                                    </StructuredListCell>
+                                    <StructuredListCell
+                                        head
+                                        noWrap={false}
+                                    >
+                                        Transport
+                                    </StructuredListCell>
+                                    <StructuredListCell
+                                        head
+                                        noWrap={false}
+                                    />
+                                    </StructuredListRow>
+                                </StructuredListHead>
+                                <StructuredListBody onKeyDown={function noRefCheck(){}}>
+                                    {this.state.allData[key].map((user,i) => 
+                                    <StructuredListRow 
+                                        key={i}
+                                        head={false}
+                                        htmlFor={"row-" + i}
+                                        label
+                                        onKeyDown={function noRefCheck(){}}
+                                        tabIndex={0}
+                                    >
+                                        <StructuredListCell
+                                            head={false}
+                                            noWrap={false}
+                                        >
+                                            {i + 1}
+                                        </StructuredListCell>
+                                        <StructuredListCell
+                                            head={false}
+                                            noWrap={false}
+                                        >
+                                            {user.fname}
+                                        </StructuredListCell>
+                                        <StructuredListCell
+                                            head={false}
+                                            noWrap={false}
+                                        >
+                                            {user.lname}
+                                        </StructuredListCell>
+                                        <StructuredListCell
+                                            head={false}
+                                            noWrap={false}
+                                        >
+                                            {user.transportation}
+                                        </StructuredListCell>
+                                        <StructuredListInput
+                                            defaultChecked={null}
+                                            id={"row-" + i}
+                                            name="row-0"
+                                            onChange={function noRefCheck(){}}
+                                            title={"row-" + i}
+                                            value={"row-" + i}
+                                        />
+                                        <StructuredListCell
+                                            head={false}
+                                            noWrap={false}
+                                        >
+                                            <svg focusable="false" preserveAspectRatio="xMidYMid meet" aria-label="select an option" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" role="img" class="bx--structured-list-svg" style={{"willChange": "transform"}}>
+                                                <path d="M8,1C4.1,1,1,4.1,1,8c0,3.9,3.1,7,7,7s7-3.1,7-7C15,4.1,11.9,1,8,1z M7,11L4.3,8.3l0.9-0.8L7,9.3l4-3.9l0.9,0.8L7,11z"></path>
+                                                <path d="M7,11L4.3,8.3l0.9-0.8L7,9.3l4-3.9l0.9,0.8L7,11z" data-icon-path="inner-path" opacity="0"></path>
+                                                <title>select an option</title>
+                                            </svg>
+                                        </StructuredListCell>
+                                    </StructuredListRow>)}
+                                </StructuredListBody>
+                            </StructuredListWrapper>
+                        </AccordionItem>))}
                     </Accordion>
 
                 </div>
              
-
-                {this.state.clickCheckin && (
-                    <CheckinPage> </CheckinPage>
-                )}
-
             </div>
 
             </div>
